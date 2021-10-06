896709e4ed2dce8b1859dbff19a81eefcb252bac
ErickBuster <91999629+ErickBuster@users.noreply.github.com>
2021-10-06 01:17:24 -0500

    Update entorno.sh

--- entorno.sh
+++ entorno.sh
@@ -36,9 +36,9 @@ mkdir $HOME/.config/bspwm
 mkdir $HOME/.config/bspwm/scripts
 mkdir $HOME/.config/sxhkd
 	#copiando archivos en sus respectivas carpetas
-cp $HOME/Descargas/entorno/archivosConfiguracion/bspwn/bspwmrc $HOME/.config/bspwm
-cp -r $HOME/Descargas/entorno/archivosConfiguracion/bspwn/scripts $HOME/.config/bspwm/
-cp    $HOME/Descargas/entorno/archivosConfiguracion/sxhdk/sxhdkrc $HOME/.config/sxhkd
+sudo cp $HOME/Descargas/entorno/archivosConfiguracion/bspwn/bspwmrc $HOME/.config/bspwm
+sudo cp -r $HOME/Descargas/entorno/archivosConfiguracion/bspwn/scripts $HOME/.config/bspwm/
+sudo cp    $HOME/Descargas/entorno/archivosConfiguracion/sxhdk/sxhdkrc $HOME/.config/sxhkd
 	#agregando permisos
 chmod +x $HOME/.config/bspwm/bspwmrc
 chmod +x $HOME/.config/bspwm/scripts/bspwm_resize
