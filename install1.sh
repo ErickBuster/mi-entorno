224f7437b5b348ea309490c73e49323726738d6d
ErickBuster <91999629+ErickBuster@users.noreply.github.com>
2021-10-06 18:08:33 -0500

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -51,7 +51,7 @@ sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb
 sudo apt update
 sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
 apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
-
+sudo apt install polybar -y
 cd $HOME/Descargas/entorno
 git clone --recursive https://github.com/polybar/polybar
 cd polybar/
