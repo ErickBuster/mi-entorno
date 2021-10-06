c85097bab933d4ec8219b34b058d28b73db72126
ErickBuster <91999629+ErickBuster@users.noreply.github.com>
2021-10-06 11:37:05 -0500

    Create install 3.sh

---
+++ install 3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "INSTALANDO POLYBAR"
+sleep 5
+#instalacion de la Polybar
+cd $HOME/Descargas/entorno
+git clone https://github.com/VaughnValle/blue-sky.git
+cd blue-sky/polybar/
+mkdir $HOME/.config/polybar
+cp * -r $HOME/.config/polybar
+
+#Descomentar en caso de que el archivo $HOME/.config/bspwm/bspwmrc no tenga el contenido
+#echo '~/.config/polybar/./launch.sh' >> $HOME/.config/bspwm/bspwmrc
+cd fonts
+sudo cp * /usr/share/fonts/truetype/
+fc-cache -v
+echo "Presionamos Win + Shift + R para ver la Polybar"
+echo "Terminado"
